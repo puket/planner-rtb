925b849bb41bcddd9584f5f3f53c88c0922a095c
Khaninnat Puket <npuket@gmail.com>
2026-04-17 17:44:14 +0700

    Update footer.php

--- app_pk/views/footer.php
+++ app_pk/views/footer.php
@@ -1,10 +1,6 @@
 <footer class="blog-footer">
-      <p>&copy; 2022 Rotbit.com <span style="color:#666;"> | Memory usage: <?php $rmUsed = number_format(((memory_get_usage()/1024)/1024), 2, '.', ''); echo $rmUsed;?> Mb.</span></p>
-      <p>
-        <a href="https://go.puketstores.com/link/gvCW4M" target="_blank" rel="nofollow noopener">Shopee.co.th</a> | <a href="https://go.puketstores.com/link/sx81CY" target="_blank" rel="nofollow noopener">Lazada.co.th</a>
-        <br>
-        <a href="#">Back to top</a>
-      </p>
+      <p>&copy; 2026 Planner @ Rotbit.com <span style="color:#666;"> | Memory usage: <?php $rmUsed = number_format(((memory_get_usage()/1024)/1024), 2, '.', ''); echo $rmUsed;?> Mb.</span></p>
+      <p>a href="#">Back to top</a></p>
     </footer>
 
 
