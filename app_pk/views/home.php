c72c8a314ae74324ef67caefa61a2edda71d0c5e
Khaninnat Puket <npuket@gmail.com>
2026-04-17 17:39:27 +0700

    Create home.php

---
+++ app_pk/views/home.php
@@ -0,0 +1,13 @@
+<main role="main" class="container">
+  <div class="row">
+          <div class="col-md-12 blog-main">
+
+            <h2>Dashboards</h2>
+            <div class="col-md-12 row" style="margin-bottom: 15px;">
+              <div class="table-responsive">
+              	
+              </div>              
+            </div>
+        </div>
+  </div>
+</main>
