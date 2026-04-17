bd7034f89ea4b6814fe2cfd568ccc9b62c68cc10
Khaninnat Puket <npuket@gmail.com>
2026-04-17 17:44:17 +0700

    Update inc_header.php

--- app_pk/views/inc_header.php
+++ app_pk/views/inc_header.php
@@ -2,10 +2,10 @@
   <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
-        <a class="text-muted" href="<?php echo base_url('dashb');?>">Dashboard</a>
+        
       </div>
       <div class="col-4 text-center">
-        <a class="blog-header-logo text-dark" href="<?php echo base_url('dashboard');?>" title="Shopping at Puket store">Admin</a>
+        <a class="blog-header-logo text-dark" href="<?php echo base_url('dashboard');?>" title="PLANNNER">PLANNNER</a>
       </div>
       <div class="col-4 d-flex justify-content-end align-items-center">
         <?php /* <a class="text-muted" href="#">
@@ -19,18 +19,9 @@
   
   <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between" style=" font-weight: bold; ">
-      <a class="p-2 text-muted1" href="<?php echo base_url('shorturl/helper');?>">Helper</a>
+      <a class="p-2 text-muted1" href="<?php echo base_url('home');?>">Home</a>
       <a class="p-2 text-muted1" href="#">|</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('shorturl');?>">Short urls</a>
-      <a class="p-2 text-muted1" href="#">|</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('admlazada/lazada_voucher');?>?slug=">LZ Campaign</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('prods/lz_prodbyhtml');?>">LZ Html</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('prods/lz_prodbyhand');?>">LZ Urls  </a>
-      <?php /* <a class="p-2 text-muted1" href="<?php echo base_url('prods/lz_shopbyhtml');?>">Shop LZ </a> */?>
-      <a class="p-2 text-muted1" href="#">|</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('admshopee/shopee_campaign');?>">SP Campaign</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('prods/sp_prodbyhtml');?>">SP Html</a>
-      <a class="p-2 text-muted1" href="<?php echo base_url('prods/sp_prodbyhand');?>">SP Urls</a>
+      <a class="p-2 text-muted1" href="<?php echo base_url('contact');?>">Contact us</a>
     </nav>
   </div>  
 </div>
