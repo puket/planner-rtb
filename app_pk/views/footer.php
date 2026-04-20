fa59d0ba811de5fab93ae749db1bfc243d149ba6
Ake Khaninnat <khaninnat@peopleplus.co.th>
2026-04-20 10:14:17 +0700

    footer

--- app_pk/views/footer.php
+++ app_pk/views/footer.php
@@ -1,8 +1,6 @@
 <footer class="blog-footer">
       <p>&copy; 2026 Planner @ Rotbit.com <span style="color:#666;"> | Memory usage: <?php $rmUsed = number_format(((memory_get_usage()/1024)/1024), 2, '.', ''); echo $rmUsed;?> Mb.</span></p>
-      <p>a href="#">Back to top</a></p>
+      <p><a href="#">Back to top</a></p>
     </footer>
-
-
   </body>
 </html>
